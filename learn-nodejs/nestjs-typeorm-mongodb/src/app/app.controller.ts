37aeadedc663746aae5ed24c767198d72e87ef05
krikchaip <krikchai.p@gmail.com>
2025-10-16 18:50:33 +0700

    nestjs-typeorm-mongodb: playaround with the common decorators

--- learn-nodejs/nestjs-typeorm-mongodb/src/app/app.controller.ts
+++ learn-nodejs/nestjs-typeorm-mongodb/src/app/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Ip, Query, Req, Res } from '@nestjs/common';
+import type { Request, Response } from 'express';
 
 import { AppService } from './app.service';
 
@@ -7,7 +8,18 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(
+    @Req() request?: Request,
+    @Ip() ip?: string,
+    @Query() query?: Record<string, any>,
+    @Res({ passthrough: true }) response?: Response,
+  ): string {
+    console.log(`request url: ${request?.url}`);
+    console.log(`request ip: ${ip}`);
+    console.log(`request query: ${JSON.stringify(query)}`);
+
+    response?.cookie('nestjs-controller', this.constructor.name);
+
     return this.appService.getHello();
   }
 }
