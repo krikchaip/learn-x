a1747e0a7af643099ab25e0746450c9fe768aada
krikchaip <krikchai.p@gmail.com>
2025-10-22 04:35:29 +0700

    nestjs-typeorm-mongodb: fix test case

--- learn-nodejs/nestjs-typeorm-mongodb/src/app/app.controller.spec.ts
+++ learn-nodejs/nestjs-typeorm-mongodb/src/app/app.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { CommonModule } from 'src/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,6 +10,7 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [CommonModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
