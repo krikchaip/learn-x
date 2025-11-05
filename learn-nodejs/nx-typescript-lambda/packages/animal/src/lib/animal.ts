0a79f65b04cdf64d51331ac2ae23742e7462b7e9
krikchaip <krikchai.p@gmail.com>
2025-11-06 02:11:28 +0700

    nx-typescript-lambda: refactor 'zoo' and 'animal' to use functions from 'util'

--- learn-nodejs/nx-typescript-lambda/packages/animal/src/lib/animal.ts
+++ learn-nodejs/nx-typescript-lambda/packages/animal/src/lib/animal.ts
@@ -1,3 +1,5 @@
+import { getRandomItem } from '@nx-typescript-lambda/util';
+
 export function animal(): string {
   return 'animal';
 }
@@ -14,5 +16,5 @@ const animals: Animal[] = [
 ];
 
 export function getRandomAnimal(): Animal {
-  return animals[Math.floor(Math.random() * animals.length)];
+  return getRandomItem(animals);
 }
--- learn-nodejs/nx-typescript-lambda/packages/zoo/src/lib/zoo.ts
+++ learn-nodejs/nx-typescript-lambda/packages/zoo/src/lib/zoo.ts
@@ -1,6 +1,9 @@
 import { getRandomAnimal } from '@nx-typescript-lambda/animal';
+import { formatMessage } from '@nx-typescript-lambda/util';
 
 export function zoo(): string {
   const animal = getRandomAnimal();
-  return `${animal.name} says ${animal.sound}`;
+  const message = `${animal.name} says ${animal.sound}`;
+
+  return formatMessage('ZOO', message);
 }
