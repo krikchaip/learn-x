b972c52c6e6b8be311d875b1d5009436f27bbc2c
krikchaip <krikchai.p@gmail.com>
2025-12-10 11:40:51 +0700

    learn-k6: add basic test case

--- learn-k6/get-started/README.md
+++ learn-k6/get-started/README.md
@@ -9,7 +9,11 @@ bun install
 To run:
 
 ```bash
+# run with configurations defined in the file
 k6 run tests/<your_test_case>.ts
+
+# run with cli options
+k6 run --vus 10 --duration 30s tests/<your_test_case>.ts
 ```
 
 This project was created using `bun init` in bun v1.3.1. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
--- learn-k6/get-started/tests/basic.ts
+++ learn-k6/get-started/tests/basic.ts
@@ -1,3 +1,14 @@
+import { sleep } from "k6";
+
+import { type Options } from "k6/options";
+import http from "k6/http";
+
+export const options: Options = {
+  iterations: 10, // execute the default function 10 times (1 VU)
+};
+
+// wait for 1-second delay between requests
 export default function () {
-  console.log("Hello via Bun!");
+  http.get("https://quickpizza.grafana.com");
+  sleep(1);
 }
